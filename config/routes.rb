7a01c19a47eca0845eee35854eef21f97906c5df
henergy-dev <44502332+henergy-dev@users.noreply.github.com>
2019-02-04 18:46:49 +0100

    Updated routes.rb to avoid the 'match' notice.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 RedmineApp::Application.routes.draw do
-	match 'projects/:project_id/newissuealerts/:action', :controller => 'newissuealerts'
-	match 'projects/:project_id/newissuealerts/:action/:id', :controller => 'newissuealerts'
+	get 'projects/:project_id/newissuealerts/:action', :controller => 'newissuealerts'
+	get 'projects/:project_id/newissuealerts/:action/:id', :controller => 'newissuealerts'
 end
